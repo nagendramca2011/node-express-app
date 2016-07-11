fe34d3fae82fb9dffb75ab7b18d67325c042b9dd
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-11 11:32:27 +0530

    template design changed

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 8000;
+app.listen(port, function(){
+	console.log('listening on', port);
+});
